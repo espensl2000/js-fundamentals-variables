22acf8ff2c0219c6116a20fce478f2ca691271d1
Espen Luna <espensluna@gmail.com>
2024-09-20 09:05:17 +0200

    Core & extension

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -3,4 +3,8 @@ let b = 10
 
 // TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
 
+a = a + b
+b = a - b
+a = a - b
+
 module.exports = { a, b }
